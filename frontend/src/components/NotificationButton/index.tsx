3b74aa4cb7308004248a3871fb4c43f2d6594bc5
Joao meneses <joaovic2012@hotmail.com>
2022-09-06 19:49:09 -0300

    Other components

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,14 +1,13 @@
-import icon from '../../assets/img/Notification-icon.svg'
+import icon from '../../assets/img/notification-icon.svg'
 
 import "./styles.css"
 
 function NotificationButton() {
-    return(
-        <div className="dsmeta-red-btn">
-                          <img src={icon} alt="Notificar" />
-                        </div>
-    )
-    }
-  
-  export default NotificationButton
-  
+  return (
+    <div className="dsmeta-red-btn">
+      <img src={icon} alt="Notificar" />
+    </div>
+  )
+}
+
+export default NotificationButton

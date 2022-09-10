ede8c8b9dfc719b1b48fdb4993b8ef6e56422af8
Joao meneses <joaovic2012@hotmail.com>
2022-09-10 17:09:02 -0300

    Teste

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { toast } from "react-toastify";
-import icon from '../../assets/img/notification-icon.svg';
+import icon from "../../assets/img/notification-icon.svg";
 import { BASE_URL } from "../../utils/request";
 
 import "./styles.css"
@@ -15,8 +15,7 @@ function handleClick(id: number) {
     toast.info("SMS enviado com sucesso")
    })
 }
-
-function NotificationButton({ saleId }: Props) {
+function NotificationButton({saleId}: Props) {
   return (
     <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
       <img src={icon} alt="Notificar" />
